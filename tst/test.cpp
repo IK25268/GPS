52d018d7765927b40ca987ba39cdd44017547291
IK25268 <162690760+IK25268@users.noreply.github.com>
2024-08-05 14:31:27 +0700

    GPS1.0

--- tst/test.cpp
+++ tst/test.cpp
@@ -12,7 +12,7 @@
 
 TEST(TestCalcValues, TestDistance) {
 	
-	Memory memory = RWFile::ReadFile("C:\\Users\\jayka\\CLionProjects\\4block\\tst\\input.txt");
+	Memory memory = RWFile::ReadFile("../tst/input.txt");
 	Handler handler;
 	handler.CalcValues(memory, 0.3);
 	ASSERT_STREQ(std::to_string(handler.GetDistance()).data(), "11808.739638");
@@ -20,7 +20,7 @@ TEST(TestCalcValues, TestDistance) {
 
 TEST(TestCalcValues, TestAverSpeed) {
 	
-	Memory memory = RWFile::ReadFile("C:\\Users\\jayka\\CLionProjects\\4block\\tst\\input.txt");
+	Memory memory = RWFile::ReadFile("../tst/input.txt");
 	Handler handler;
 	handler.CalcValues(memory, 0.3);
 	ASSERT_STREQ(std::to_string(handler.GetAverSpeed()).data(), "2.986530");
@@ -28,7 +28,7 @@ TEST(TestCalcValues, TestAverSpeed) {
 
 TEST(TestCalcValues, TestMoutTime) {
 	
-	Memory memory = RWFile::ReadFile("C:\\Users\\jayka\\CLionProjects\\4block\\tst\\input.txt");
+	Memory memory = RWFile::ReadFile("../tst/input.txt");
 	Handler handler;
 	handler.CalcValues(memory, 0.3);
 	ASSERT_STREQ(std::to_string(handler.GetMoutTime()).data(), "3899");
@@ -36,7 +36,7 @@ TEST(TestCalcValues, TestMoutTime) {
 
 TEST(TestCalcValues, TestStopTime) {
 	
-	Memory memory = RWFile::ReadFile("C:\\Users\\jayka\\CLionProjects\\4block\\tst\\input.txt");
+	Memory memory = RWFile::ReadFile("../tst/input.txt");
 	Handler handler;
 	handler.CalcValues(memory, 0.3);
 	ASSERT_STREQ(std::to_string(handler.GetStopTime()).data(), "55");
@@ -44,7 +44,7 @@ TEST(TestCalcValues, TestStopTime) {
 
 TEST(TestCalcValues, TestAverMoutSpeed) {
 	
-	Memory memory = RWFile::ReadFile("C:\\Users\\jayka\\CLionProjects\\4block\\tst\\input.txt");
+	Memory memory = RWFile::ReadFile("../tst/input.txt");
 	Handler handler;
 	handler.CalcValues(memory, 0.3);
 	ASSERT_STREQ(std::to_string(handler.GetAverMoutSpeed()).data(), "3.028659");
@@ -52,7 +52,7 @@ TEST(TestCalcValues, TestAverMoutSpeed) {
 
 TEST(TestCalcValues, TestMaxSpeed) {
 	
-	Memory memory = RWFile::ReadFile("C:\\Users\\jayka\\CLionProjects\\4block\\tst\\input.txt");
+	Memory memory = RWFile::ReadFile("../tst/input.txt");
 	Handler handler;
 	handler.CalcValues(memory, 0.3);
 	ASSERT_STREQ(std::to_string(handler.GetMaxSpeed()).data(), "10.600183");
@@ -60,7 +60,7 @@ TEST(TestCalcValues, TestMaxSpeed) {
 
 TEST(TestCalcValues, TestMinAltit) {
 	
-	Memory memory = RWFile::ReadFile("C:\\Users\\jayka\\CLionProjects\\4block\\tst\\input.txt");
+	Memory memory = RWFile::ReadFile("../tst/input.txt");
 	Handler handler;
 	handler.CalcValues(memory, 0.3);
 	ASSERT_STREQ(std::to_string(handler.GetMinAltit()).data(), "544.372799");
@@ -68,7 +68,7 @@ TEST(TestCalcValues, TestMinAltit) {
 
 TEST(TestCalcValues, TestMaxAltit) {
 	
-	Memory memory = RWFile::ReadFile("C:\\Users\\jayka\\CLionProjects\\4block\\tst\\input.txt");
+	Memory memory = RWFile::ReadFile("../tst/input.txt");
 	Handler handler;
 	handler.CalcValues(memory, 0.3);
 	ASSERT_STREQ(std::to_string(handler.GetMaxAltit()).data(), "605.332799");
@@ -76,7 +76,7 @@ TEST(TestCalcValues, TestMaxAltit) {
 
 TEST(TestCalcValues, TestSummAscend) {
 	
-	Memory memory = RWFile::ReadFile("C:\\Users\\jayka\\CLionProjects\\4block\\tst\\input.txt");
+	Memory memory = RWFile::ReadFile("../tst/input.txt");
 	Handler handler;
 	handler.CalcValues(memory, 0.3);
 	ASSERT_STREQ(std::to_string(handler.GetSummAscend()).data(), "508.711199");
@@ -84,7 +84,7 @@ TEST(TestCalcValues, TestSummAscend) {
 
 TEST(TestCalcValues, TestSummDescend) {
 	
-	Memory memory = RWFile::ReadFile("C:\\Users\\jayka\\CLionProjects\\4block\\tst\\input.txt");
+	Memory memory = RWFile::ReadFile("../tst/input.txt");
 	Handler handler;
 	handler.CalcValues(memory, 0.3);
 	ASSERT_STREQ(std::to_string(handler.GetSummDescend()).data(), "495.299999");
@@ -92,7 +92,7 @@ TEST(TestCalcValues, TestSummDescend) {
 
 TEST(TestCalcValues, TestDistribSpd) {
 	
-	Memory memory = RWFile::ReadFile("C:\\Users\\jayka\\CLionProjects\\4block\\tst\\input.txt");
+	Memory memory = RWFile::ReadFile("../tst/input.txt");
 	memory.AddSpdRange({ 0.0, 4.0 });
 	memory.AddSpdRange({ 5.0, 7.0 });
 	memory.AddSpdRange({ 8.0, 20.0 });
